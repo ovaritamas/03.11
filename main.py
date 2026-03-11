688b29d1123282413367009b1be426a590d67479
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2026-03-11 08:46:44 +0100

    3.feladat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from allat import Allat
-from emlos import Emlos
+from emlos import Emlos, Kutya, Macska
 
 allat_1 = Allat("Bodri", "kutya", 3, "ház", "közepes")
 allat_2 = Allat("Cirmi", "macska", 2, "ház", "kicsi")
@@ -9,3 +9,6 @@ emlos = Emlos("Morzsi", "kutya", 3, "ház", "közepes", "barna")
 print(allat_1)
 print(allat_2)
 print(emlos)
+
+morzsi = Kutya("Morzsi", 5, "ház", "barna")
+hubert = Macska("Hubert", 2, "ház", "fekete")
