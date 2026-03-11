9991d8ad1ffaba48d3f7bfc8e05d95a2bb3bb98e
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2026-03-11 08:22:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ allat_1 = Allat("Bodri", "kutya", 3, "ház", "közepes")
 allat_2 = Allat("Cirmi", "macska", 2, "ház", "kicsi")
 
 emlos = Emlos("Morzsi", "kutya", 3, "ház", "közepes", "barna")
+
+print(allat_1)
+print(allat_2)
+print(emlos)
