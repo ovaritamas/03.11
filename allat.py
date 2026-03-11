42d53c6a8541f8ae88acb7cc8a07ef79c0b7801a
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2026-03-11 07:47:26 +0100

    1.ora

---
+++ allat.py
@@ -0,0 +1,11 @@
+class Allat:
+    def __init__(self, nev_, faj_, eletkor_, elohely_, meret_):
+        self.nev = nev_
+        self.faj = faj_
+        self.eletkor = eletkor_
+        self.elohely = elohely_
+        self.meret = meret_
+
+    def __str__(self):
+        return f"{self.nev} {self.faj} {self.eletkor} éves, élőhelye {self.elohely}"
+        
---
+++ main.py
@@ -0,0 +1,4 @@
+from allat import Allat
+
+allat_1 = Allat("Bodri", "kutya", 3, "ház", "közepes")
+allat_2 = Allat("Cirmi", "macska", 2, "ház", "kicsi")
